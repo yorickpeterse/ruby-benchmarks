008d12d299e770782343ade32ec9006448c976e2
Yorick Peterse <yorickpeterse@gmail.com>
2012-01-23 13:41:29 +0100

    Expanded the proc vs lambda benchmark.
    Signed-off-by: Yorick Peterse <yorickpeterse@gmail.com>

--- proc_vs_lambda/bench.rb
+++ proc_vs_lambda/bench.rb
@@ -1,21 +1,42 @@
 require 'benchmark'
 
+dummy_instance     = Struct.new(:name).new(nil)
+instance_eval_proc = proc do
+  @name = 'Foobar'
+end
+
+instance_eval_lambda = lambda do |instance|
+  @name = 'Foobar'
+end
+
 if ARGV[0] and !ARGV[0].empty?
   amount = ARGV[0].to_i
 else
   amount = 1000000
 end
 
-Benchmark.bmbm(20) do |bench|
-  bench.report('Proc.new') do
+Benchmark.bmbm(30) do |bench|
+  bench.report('Proc.new#call') do
     amount.times do
       Proc.new { 10+15 }.call
     end
   end
 
-  bench.report('lambda') do
+  bench.report('lambda#call') do
     amount.times do
       lambda { 10+15 }.call
+    end
+  end
+
+  bench.report('instance_eval with Proc.new') do
+    (amount * 10).times do
+      dummy_instance.instance_eval(&instance_eval_proc)
+    end
+  end
+
+  bench.report('instance_eval with lambda') do
+    (amount * 10).times do
+      dummy_instance.instance_eval(&instance_eval_lambda)
     end
   end
 end
